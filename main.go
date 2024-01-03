2ecb92dd0dd9c0ead3b97e87d2db8e62b57537c6
danxguerrero <danxguerrero@yahpp.com>
2024-01-02 23:41:37 -0500

    Displays temp for entered city

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	var city string
+	fmt.Print("Enter City: ")
+	fmt.Scanln(&city)
+
+	weatherData, err := getWeatherCity(city)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	// Extract and print relevant information
+	fmt.Println("---", city, "---")
+	fmt.Printf("Current Temperature: %v°C\n", weatherData["main"].(map[string]interface{})["temp"])
+}
+
+func getWeatherCity(city string) (map[string]interface{}, error) {
+	apiKey := os.Getenv("OPENWEATHERMAP_API_KEY")
+	// build API url
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, apiKey)
+	
+	// Make the API call
+	res, err := http.Get(url)
+	if err != nil{
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	// Read response body
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Parse JSON response
+	var data map[string]interface{}
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
